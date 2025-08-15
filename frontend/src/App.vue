03bcc7c2e90bb98ec3eb3d5cbc9ba55005e1219a
Cause <165661145+Cause-01@users.noreply.github.com>
2025-08-15 08:47:49 +0800

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <nav>
+      <ul>
+        <li><router-link to="/">运输订单</router-link></li>
+        <li><router-link to="/vehicles">车辆管理</router-link></li>
+      </ul>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -10,6 +16,19 @@ export default {
 };
 </script>
 
-<style>
-/* Add your styles here */
+<style scoped>
+nav {
+  margin: 20px;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+}
+
+ul {
+  list-style: none;
+  display: flex;
+}
+
+li {
+  margin-right: 15px;
+}
 </style>
